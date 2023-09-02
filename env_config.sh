2abfc7eac8f6f679d0308297e583de784ee7fc49
maktukmak <mehmet.aktukmak@intel.com>
2023-09-02 14:58:25 -0700

    env config added

---
+++ env_config.sh
@@ -0,0 +1,8 @@
+conda create -n gcp python=3.10
+
+pip3 install torch torchvision torchaudio
+
+pip install torchserve torch-model-archiver torch-workflow-archiver
+
+git clone https://github.com/naoto0804/pytorch-AdaIN.git
+
